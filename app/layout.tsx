4ede70cf6f073e19ff75dc36782e52e673a96d04
Obasan Tomi <obasantomilola@gmail.com>
2026-04-26 19:53:12 +0100

    update url for og-image.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/icon.svg",
+        url: "https://analytica-neon.vercel.app/og-image.png",
         width: 1200,
         height: 630,
         alt: "Analytica",
