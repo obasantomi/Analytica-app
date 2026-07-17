3a5042a9ae3e35891824175e5411eefe59f2ada4
Obasan Tomilola <obasantomilola@gmail.com>
2026-07-17 14:32:58 +0100

    reconfigure auth-page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className="flex justify-center min-h-screen shadow-[0_40px_120px_rgba(0,0,0,0.12)] bg-[#C4C6D04D] p-3 md:p-12 text-white">
+    <div className="md:flex justify-center min-h-screen shadow-[0_40px_120px_rgba(0,0,0,0.12)] bg-white p-3 md:p-12 text-white">
       <div className="flex rounded-lg overflow-hidden">
         <div className="max-w-170 hidden md:block w-full h-full">
           <AuthLayoutStyle />
